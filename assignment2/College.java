57fc0b13d3590364e7f1e1e8324b0b52591115cf
Poornima K <70528254+Poornima0108@users.noreply.github.com>
2022-03-05 22:42:00 +0530

    Add files via upload

---
+++ assignment2/College.java
@@ -0,0 +1,16 @@
+package org.college;
+
+public class College {
+
+	public void collegeName() {
+		System.out.println("College name: SASTRA");
+	}
+	
+	public void collegeCode() {
+		System.out.println("College code: 123");
+	}
+	
+	public void collegeRank() {
+		System.out.println("College rank: 14");
+	}
+}
