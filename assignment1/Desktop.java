7f8d9d49a45a8c312936322faf73ef7f754988ce
Poornima K <70528254+Poornima0108@users.noreply.github.com>
2022-03-05 22:37:54 +0530

    Add files via upload

---
+++ assignment1/Desktop.java
@@ -0,0 +1,15 @@
+package org.system;
+
+public class Desktop extends Computer {
+
+	public void desktopSize() {
+		System.out.println("Desktop size: 18 inch");
+	}
+	public static void main(String[] args) {
+		Desktop d = new Desktop();
+		d.computerModel();
+		d.desktopSize();
+
+	}
+
+}
